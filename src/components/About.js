40655b2719408707b1e2cf679425a77b7b243f97
bradleymbuvi <bradwent@gmail.com>
2024-02-07 14:02:27 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,12 @@ import { image } from "../data/data";
 
 function About() {
   return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Hey ...</p>
+    <img src={image} alt="I made this"></img>
+
+  </div>
 }
 
 export default About;
